2781daf4d3f3ae510fbe47ff9db156d9b63fdea0
yair <157630605+Yair-Dekel@users.noreply.github.com>
2026-08-04 18:30:05 +0300

    feat(redis): add stub function

--- backend/inc/repository/RedisCacheRepository.hpp
+++ backend/inc/repository/RedisCacheRepository.hpp
@@ -11,6 +11,12 @@ public:
     RedisCacheRepository(const std::string& host, int port);
     ~RedisCacheRepository() override = default;
 
+    void update_spot_occupancy(
+        const std::string& lot_id,
+        const std::string& spot_id,
+        bool is_occupied
+    ) override;
+
 private:
     std::string host_;
     int port_ = 0;
