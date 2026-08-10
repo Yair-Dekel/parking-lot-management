fac7d4b5cfdedf660448996435e0c9e72a1ab2b3
root <root@LAPTOP-U121KN2O>
2026-08-10 21:41:24 +0300

    main server parsing data from the gate

--- backend/inc/server/MainServer.hpp
+++ backend/inc/server/MainServer.hpp
@@ -8,6 +8,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <cstring>
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
 
 #define SERVER_PORT 5000
 
@@ -15,52 +18,59 @@ inline constexpr const char DEFAULT_REDIS_HOST[] = "localhost";
 inline constexpr int DEFAULT_REDIS_PORT = 6379;
 inline constexpr const char DEFAULT_MQTT_HOST[] = "localhost";
 inline constexpr int DEFAULT_MQTT_PORT = 1883;
+constexpr uint32_t ENTRY_REQUEST_PAYLOAD_SIZE = sizeof(uint32_t) * 3 + sizeof(uint8_t) * 2;
+constexpr std::size_t PARKING_LOT_ID_OFFSET = 4;
+constexpr std::size_t GATE_ID_OFFSET = 8;
+constexpr std::size_t WANTS_HANDICAP_OFFSET = 12;
+constexpr std::size_t WANTS_ELECTRIC_OFFSET = 13;
 
 struct mosquitto;
 struct mosquitto_message;
 
-namespace parkpulse {
+namespace parkpulse
+{
 
-class ICacheRepository;
+    class ICacheRepository;
 
-class MainServer {
-public:
-    MainServer(
-        int port = SERVER_PORT,
-        std::string redis_host = DEFAULT_REDIS_HOST,
-        int redis_port = DEFAULT_REDIS_PORT,
-        std::string mqtt_host = DEFAULT_MQTT_HOST,
-        int mqtt_port = DEFAULT_MQTT_PORT
-    );
-    ~MainServer();
+    class MainServer
+    {
+    public:
+        MainServer(
+            int port = SERVER_PORT,
+            std::string redis_host = DEFAULT_REDIS_HOST,
+            int redis_port = DEFAULT_REDIS_PORT,
+            std::string mqtt_host = DEFAULT_MQTT_HOST,
+            int mqtt_port = DEFAULT_MQTT_PORT);
+        ~MainServer();
 
-    void start();
-    void stop();
+        void start();
+        void stop();
 
-private:
-    void setupSocket();
-    void setup_mqtt();
-    void run();
-    void acceptClient();
-    void receiveMessage(int client_fd);
-    void handle_message(const char* buffer, int bytes, int client_fd);
-    void handle_mqtt_message(const std::string& payload);
+    private:
+        void setupSocket();
+        void setup_mqtt();
+        void run();
+        void acceptClient();
+        void receiveMessage(int client_fd);
+        void handle_message(const char *payload, int client_fd);
+        void handle_mqtt_message(const std::string &payload);
 
-    static void on_mqtt_message(mosquitto* mosq, void* obj, const mosquitto_message* message);
+        static void on_mqtt_message(mosquitto *mosq, void *obj, const mosquitto_message *message);
 
-private:
-    std::unique_ptr<ICacheRepository> cache_repository_;
+    private:
+        std::unique_ptr<ICacheRepository> cache_repository_;
+        std::unordered_map<int, std::vector<char>> input_buffers_; // Ron: mapping client_fds to buffers
 
-    int port_ = SERVER_PORT;
-    std::string redis_host_ = DEFAULT_REDIS_HOST;
-    int redis_port_ = DEFAULT_REDIS_PORT;
-    std::string mqtt_host_ = DEFAULT_MQTT_HOST;
-    int mqtt_port_ = DEFAULT_MQTT_PORT;
+        int port_ = SERVER_PORT;
+        std::string redis_host_ = DEFAULT_REDIS_HOST;
+        int redis_port_ = DEFAULT_REDIS_PORT;
+        std::string mqtt_host_ = DEFAULT_MQTT_HOST;
+        int mqtt_port_ = DEFAULT_MQTT_PORT;
 
-    int server_fd_ = -1;
-    int epoll_fd_ = -1;
+        int server_fd_ = -1;
+        int epoll_fd_ = -1;
 
-    mosquitto* mqtt_client_ = nullptr;
-};
+        mosquitto *mqtt_client_ = nullptr;
+    };
 
 } // namespace parkpulse
--- backend/src/server/MainServer.cpp
+++ backend/src/server/MainServer.cpp
@@ -15,242 +15,316 @@
 #define MQTT_BROKER_PORT 1883
 #define MQTT_TOPIC_FILTER "parking/+/+/spot_update"
 
-namespace parkpulse {
-
-MainServer::MainServer(int port, std::string redis_host, int redis_port, std::string mqtt_host, int mqtt_port)
-    : port_(port)
-    , redis_host_(std::move(redis_host))
-    , redis_port_(redis_port)
-    , mqtt_host_(std::move(mqtt_host))
-    , mqtt_port_(mqtt_port)
+namespace parkpulse
 {
-}
 
-MainServer::~MainServer()
-{ 
-    stop(); 
-}
-
-void MainServer::start() 
-{
-    cache_repository_ = std::make_unique<RedisCacheRepository>(redis_host_, redis_port_);
-
-    setupSocket();
-    setup_mqtt();
-
-    std::cout << "ParkPulse main_server running on port " << port_
-              << " (redis=" << redis_host_ << ":" << redis_port_
-              << ", mqtt=" << mqtt_host_ << ":" << mqtt_port_
-              << "). Ctrl+C to stop.\n";
-
-    run();
-}
-
-void MainServer::stop() 
-{
-    if (mqtt_client_ != nullptr) {
-        mosquitto_disconnect(mqtt_client_);
-        mosquitto_destroy(mqtt_client_);
-        mqtt_client_ = nullptr;
-
-        mosquitto_lib_cleanup();
-    }
-    if (server_fd_ >= 0) {
-        close(server_fd_);
-        server_fd_ = -1;
+    MainServer::MainServer(int port, std::string redis_host, int redis_port, std::string mqtt_host, int mqtt_port)
+        : port_(port), redis_host_(std::move(redis_host)), redis_port_(redis_port), mqtt_host_(std::move(mqtt_host)), mqtt_port_(mqtt_port)
+    {
     }
 
-    if (epoll_fd_ >= 0) {
-        close(epoll_fd_);
-        epoll_fd_ = -1;
+    MainServer::~MainServer()
+    {
+        stop();
     }
-}
 
-// Private methods
-void MainServer::setupSocket() 
-{
-    server_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+    void MainServer::start()
+    {
+        cache_repository_ = std::make_unique<RedisCacheRepository>(redis_host_, redis_port_);
 
-    int opt = 1;
-    setsockopt(server_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+        setupSocket();
+        setup_mqtt();
 
-    sockaddr_in addr{};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port_);
-    addr.sin_addr.s_addr = INADDR_ANY;
+        std::cout << "ParkPulse main_server running on port " << port_
+                  << " (redis=" << redis_host_ << ":" << redis_port_
+                  << ", mqtt=" << mqtt_host_ << ":" << mqtt_port_
+                  << "). Ctrl+C to stop.\n";
 
-    bind(server_fd_, (sockaddr*)&addr, sizeof(addr));
-    listen(server_fd_, SOMAXCONN);
+        run();
+    }
 
-    // non blocking socket
-    fcntl(server_fd_, F_SETFL, O_NONBLOCK);
+    void MainServer::stop()
+    {
+        if (mqtt_client_ != nullptr)
+        {
+            mosquitto_disconnect(mqtt_client_);
+            mosquitto_destroy(mqtt_client_);
+            mqtt_client_ = nullptr;
 
-    epoll_fd_ = epoll_create1(0);
-
-    epoll_event event{};
-    event.events = EPOLLIN;
-    event.data.fd = server_fd_;
-
-    epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, server_fd_, &event);
-}
-
-void MainServer::setup_mqtt()
-{
-    mosquitto_lib_init();
-
-    mqtt_client_ = mosquitto_new(nullptr, true, this);
-
-    mosquitto_message_callback_set(mqtt_client_, on_mqtt_message);
-
-    mosquitto_connect(mqtt_client_, MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60);
-    mosquitto_subscribe(mqtt_client_, nullptr, MQTT_TOPIC_FILTER, 1);
-
-    int mqtt_fd = mosquitto_socket(mqtt_client_);
-
-    epoll_event event{};
-    event.events = EPOLLIN;
-    event.data.fd = mqtt_fd;
-
-    epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, mqtt_fd, &event);
-}
-
-void MainServer::run() 
-{
-    epoll_event events[10];
-
-    while (true) {
-        // Finite timeout (instead of -1) so we can service mosquitto's
-        // keepalive/misc work even when no fd is ready.
-        int count = epoll_wait(epoll_fd_, events, 10, 1000);
-
-        for (int i = 0; i < count; i++) {
-
-            int fd = events[i].data.fd;
-
-            if (fd == server_fd_) {
-                acceptClient();
-            } else if (mqtt_client_ != nullptr && fd == mosquitto_socket(mqtt_client_)) {
-                mosquitto_loop_read(mqtt_client_, 1);
-
-            } else {
-                receiveMessage(fd);
-            }
+            mosquitto_lib_cleanup();
+        }
+        if (server_fd_ >= 0)
+        {
+            close(server_fd_);
+            server_fd_ = -1;
         }
 
-        if (mqtt_client_ != nullptr) {
-            mosquitto_loop_misc(mqtt_client_);
+        if (epoll_fd_ >= 0)
+        {
+            close(epoll_fd_);
+            epoll_fd_ = -1;
+        }
+    }
 
-            if (mosquitto_want_write(mqtt_client_)) {
-                mosquitto_loop_write(mqtt_client_, 1);
+    // Private methods
+    void MainServer::setupSocket()
+    {
+        server_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+
+        int opt = 1;
+        setsockopt(server_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+        sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port_);
+        addr.sin_addr.s_addr = INADDR_ANY;
+
+        bind(server_fd_, (sockaddr *)&addr, sizeof(addr));
+        listen(server_fd_, SOMAXCONN);
+
+        // non blocking socket
+        fcntl(server_fd_, F_SETFL, O_NONBLOCK);
+
+        epoll_fd_ = epoll_create1(0);
+
+        epoll_event event{};
+        event.events = EPOLLIN;
+        event.data.fd = server_fd_;
+
+        epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, server_fd_, &event);
+    }
+
+    void MainServer::setup_mqtt()
+    {
+        mosquitto_lib_init();
+
+        mqtt_client_ = mosquitto_new(nullptr, true, this);
+
+        mosquitto_message_callback_set(mqtt_client_, on_mqtt_message);
+
+        mosquitto_connect(mqtt_client_, MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60);
+        mosquitto_subscribe(mqtt_client_, nullptr, MQTT_TOPIC_FILTER, 1);
+
+        int mqtt_fd = mosquitto_socket(mqtt_client_);
+
+        epoll_event event{};
+        event.events = EPOLLIN;
+        event.data.fd = mqtt_fd;
+
+        epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, mqtt_fd, &event);
+    }
+
+    void MainServer::run()
+    {
+        epoll_event events[10];
+
+        while (true)
+        {
+            // Finite timeout (instead of -1) so we can service mosquitto's
+            // keepalive/misc work even when no fd is ready.
+            int count = epoll_wait(epoll_fd_, events, 10, 1000);
+
+            for (int i = 0; i < count; i++)
+            {
+
+                int fd = events[i].data.fd;
+
+                if (fd == server_fd_)
+                {
+                    acceptClient();
+                }
+                else if (mqtt_client_ != nullptr && fd == mosquitto_socket(mqtt_client_))
+                {
+                    mosquitto_loop_read(mqtt_client_, 1);
+                }
+                else
+                {
+                    receiveMessage(fd);
+                }
+            }
+
+            if (mqtt_client_ != nullptr)
+            {
+                mosquitto_loop_misc(mqtt_client_);
+
+                if (mosquitto_want_write(mqtt_client_))
+                {
+                    mosquitto_loop_write(mqtt_client_, 1);
+                }
             }
         }
     }
-}
 
-void MainServer::acceptClient() 
-{
-    sockaddr_in client{};
-    socklen_t size = sizeof(client);
+    void MainServer::acceptClient()
+    {
+        sockaddr_in client{};
+        socklen_t size = sizeof(client);
 
-    int client_fd = accept(
-        server_fd_,
-        (sockaddr*)&client,
-        &size
-    );
+        int client_fd = accept(
+            server_fd_,
+            (sockaddr *)&client,
+            &size);
 
-    if (client_fd < 0) {
-        return;
+        if (client_fd < 0)
+        {
+            return;
+        }
+
+        fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+        epoll_event event{};
+        event.events = EPOLLIN;
+        event.data.fd = client_fd;
+
+        epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, client_fd, &event);
+
+        std::cout << "Client connected\n";
     }
 
-    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+    void MainServer::receiveMessage(int client_fd)
+    {
+        char buffer[1024];
 
-    epoll_event event{};
-    event.events = EPOLLIN;
-    event.data.fd = client_fd;
+        int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
 
-    epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, client_fd, &event);
+        if (bytes <= 0)
+        {
+            close(client_fd);
+            epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, client_fd, nullptr);
+            input_buffers_.erase(client_fd); // close the buffer corresponding to the client_fd
 
-    std::cout << "Client connected\n";
-}
+            std::cout << "Client disconnected\n";
+            return;
+        }
 
+        // Lines 200 - 250 added by Ron
+        // Add newly received bytes to the buffer of this gate(client_fd)
+        std::vector<char> &input_buffer = input_buffers_[client_fd];
 
-void MainServer::receiveMessage(int client_fd)
-{
-    char buffer[1024];
+        input_buffer.insert(input_buffer.end(), buffer, buffer + bytes);
 
-    int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+        // Need at least 4 bytes for payload_size
+        if (input_buffer.size() < sizeof(uint32_t))
+        {
+            return;
+        }
 
-    if (bytes <= 0) {
-        close(client_fd);
+        // "payload_size" will hold the amount of overall bytes in the message, which should be 14
+        uint32_t payload_size_network;
+        std::memcpy(&payload_size_network, input_buffer.data(), sizeof(uint32_t));
+        uint32_t payload_size = ntohl(payload_size_network);
+        if (payload_size != ENTRY_REQUEST_PAYLOAD_SIZE)
+        {
+            std::cerr << "Invalid payload size: " << payload_size << std::endl;
+            return;
+        }
 
-        epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, client_fd, nullptr);
+        const std::size_t overall_frame_size = sizeof(uint32_t) + payload_size;
 
-        std::cout << "Client disconnected\n";
-        return;
+        // Header arrived, but payload isn't complete yet
+        if (input_buffer.size() < overall_frame_size)
+        {
+            return;
+        }
+
+        // skip 4 bytes over the payload size to reach the payload itself
+        const char *payload = input_buffer.data() + sizeof(uint32_t);
+
+        handle_message(payload, client_fd);
+
+        // Remove processed frame from buffer
+        input_buffer.erase(input_buffer.begin(), input_buffer.begin() + overall_frame_size);
     }
 
-    handle_message(buffer, bytes, client_fd);
-}
+    void MainServer::handle_message(const char *payload, int client_fd)
+    {
+        (void)client_fd;
 
-void MainServer::handle_message(const char* buffer, int bytes, int client_fd)
-{
-    (void)client_fd;
-    std::string message(buffer, bytes);
-    (void)message;
-    
-    const EntryRequest* request = reinterpret_cast<const EntryRequest*>(buffer);
-    
-    std::cout << "Received EntryRequest: parking_lot_id=" << request->parking_lot_id
-              << ", gate_id=" << request->gate_id
-              << ", wants_handicap=" << request->wants_handicap
-              << ", wants_electric=" << request->wants_electric
-              << std::endl;
-    /*
-    std::string message(buffer, bytes);
+        uint32_t request_id_network;
+        uint32_t parking_lot_id_network;
+        uint32_t gate_id_network;
 
-    EntryRequest* request = reinterpret_cast<EntryRequest*>(buffer);
+        std::memcpy(&request_id_network, payload, sizeof(uint32_t));
+        std::memcpy(&parking_lot_id_network, payload + PARKING_LOT_ID_OFFSET, sizeof(uint32_t));
+        std::memcpy(&gate_id_network, payload + GATE_ID_OFFSET, sizeof(uint32_t));
 
-    */
-}
+        const uint32_t request_id = ntohl(request_id_network);
+        const uint32_t parking_lot_id = ntohl(parking_lot_id_network);
+        const uint32_t gate_id = ntohl(gate_id_network);
 
-void MainServer::on_mqtt_message(mosquitto* mosq, void* obj, const mosquitto_message* message)
-{
-    (void)mosq;
+        const bool wants_handicap = payload[WANTS_HANDICAP_OFFSET] != 0;
+        const bool wants_electric = payload[WANTS_ELECTRIC_OFFSET] != 0;
 
-    if (message->payloadlen <= 0) {
-        return;
-    }
+        std::cout
+            << "Received EntryRequest:"
+            << " request_id=" << request_id
+            << ", parking_lot_id=" << parking_lot_id
+            << ", gate_id=" << gate_id
+            << ", wants_handicap=" << wants_handicap
+            << ", wants_electric=" << wants_electric
+            << std::endl;
 
-    auto* self = static_cast<MainServer*>(obj);
+        /*
 
-    std::string payload(static_cast<const char*>(message->payload), message->payloadlen);
+        (void)client_fd;
+        std::string message(buffer, bytes);
+        (void)message;
 
-    self->handle_mqtt_message(payload);
-}
+        const EntryRequest* request = reinterpret_cast<const EntryRequest*>(buffer);
 
-void MainServer::handle_mqtt_message(const std::string& payload)
-{
-    try {
-        const nlohmann::json json_payload = nlohmann::json::parse(payload);
-
-        const std::string lot_id = json_payload.at("lot_id").get<std::string>();
-        const std::string sensor_server_id = json_payload.at("sensor_server_id").get<std::string>();
-        const Spot spot = json_payload.at("spot").get<Spot>();
-
-        std::cout << "Spot update received: lot_id=" << lot_id
-                  << ", sensor_server_id=" << sensor_server_id
-                  << ", spot_id=" << spot.id
-                  << ", taken=" << spot.taken
+        std::cout << "Received EntryRequest: parking_lot_id=" << request->parking_lot_id
+                  << ", gate_id=" << request->gate_id
+                  << ", wants_handicap=" << request->wants_handicap
+                  << ", wants_electric=" << request->wants_electric
                   << std::endl;
 
-        /*if (cache_repository_) {
-            cache_repository_->update_spot_occupancy(lot_id, spot);
-        }*/
+        std::string message(buffer, bytes);
+
+        EntryRequest* request = reinterpret_cast<EntryRequest*>(buffer);
+
+        */
     }
-    catch (const std::exception& e) {
-        std::cerr << "Failed to handle MQTT message: " << e.what() << std::endl;
+
+    void MainServer::on_mqtt_message(mosquitto *mosq, void *obj, const mosquitto_message *message)
+    {
+        (void)mosq;
+
+        if (message->payloadlen <= 0)
+        {
+            return;
+        }
+
+        auto *self = static_cast<MainServer *>(obj);
+
+        std::string payload(static_cast<const char *>(message->payload), message->payloadlen);
+
+        self->handle_mqtt_message(payload);
     }
-}
+
+    void MainServer::handle_mqtt_message(const std::string &payload)
+    {
+        try
+        {
+            const nlohmann::json json_payload = nlohmann::json::parse(payload);
+
+            const std::string lot_id = json_payload.at("lot_id").get<std::string>();
+            const std::string sensor_server_id = json_payload.at("sensor_server_id").get<std::string>();
+            const Spot spot = json_payload.at("spot").get<Spot>();
+
+            std::cout << "Spot update received: lot_id=" << lot_id
+                      << ", sensor_server_id=" << sensor_server_id
+                      << ", spot_id=" << spot.id
+                      << ", taken=" << spot.taken
+                      << std::endl;
+
+            /*if (cache_repository_) {
+                cache_repository_->update_spot_occupancy(lot_id, spot);
+            }*/
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Failed to handle MQTT message: " << e.what() << std::endl;
+        }
+    }
 
 } // namespace parkpulse
-
